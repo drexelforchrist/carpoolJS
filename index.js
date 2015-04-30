dd5f6fc73d05a2b5428be8423ada749daaf0e3a0
James Kurtz <jkrrv@jkrrv.org>
2015-04-30 16:45:15 -0400

    Execution step 2; Fixed lib refs

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+/* global require */
+
+require.config({
+    paths: {
+        'underscore': 'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min'
+    }
+});
+
+Array.prototype.contains = function(obj) {
+    var i = this.length;
+    while (i--) {
+        if (this[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
+
 require(['underscore'], function (_) {
-
-    //underscore is now loaded.
-
-
-    var numPeople = 50;
+    var numPeople = 20;
 
 
     var locations = ["Ocean City, NJ"]; // destination
@@ -24,11 +38,88 @@ require(['underscore'], function (_) {
         [104, 1, 1, 1, 1, 0]  // 3200 Chestnut St
     ];
 
+
+
+    /* COST EVALUATIONS */
     var evaluations = [
+
+        // Base.  Currently, number of different origins.
         function (car, passengers) {
-            car // KURTZ this needs to be written mad quick.
+            var origins = [];
+            passengers.forEach(function (passenger) {
+                if (!origins.contains(passenger.origin)) {
+                    origins.push(passenger.origin);
+                }
+            });
+            return origins.length + 10;
+        },
+
+
+        // Over seatbelt limit.
+        function (car, passengers) {
+            if (passengers.length > car.seats) {
+                return 10000000000000000;
+            }
+            return 1;
+        },
+
+
+        // Car Fullness.  Ideally 80%.
+        function (car, passengers) {
+            return Math.cos((passengers.length/car.seats * Math.PI+0.8)+1)*0.2+1;
+        },
+
+
+        // Car Gender Composition.  Single-gender cars are ideal, but a perfectly even split is the next-best.
+        function (car, passengers) {
+            var numerator = 0,
+                denominator = 0;
+            passengers.forEach(function (person) {
+                numerator += person.sex;
+                denominator += 1;
+            });
+
+            var sexRatio = numerator/denominator;
+
+            return 1 + 0.2 * (Math.sin(sexRatio * 2.8) + Math.abs(Math.sin(sexRatio * 2 * Math.PI)));
+        },
+
+
+        // Desire to Drive.  This is important.
+        function (car, passengers) {
+            return 2.25 - (0.25 * car.driver.wantsToDrive); // more efficient form of (5 - x) * 0.25 + 1
         }
+
     ];
+
+
+    /**
+     * The evalCost function.  Evaluates the cost of a car, with a separate given passenger list.  Iterates through each
+     * of the 'evaluations', and multiplies those results together.
+     *
+     * @param car
+     * @param passengers
+     * @returns {number}
+     */
+    function evalCost(car, passengers) {
+        var cost = 1;
+        evaluations.forEach(function(test) {
+            cost *= test(car, passengers);
+        });
+        return cost;
+    }
+
+    /**
+     * The evalTotalCost function returns the total cost for all car assignments.
+     *
+     */
+    function evalTotalCost() {
+        var cost = 0;
+        cars.forEach(function (car) {
+            cost += evalCost(car, car.passengers);
+        });
+        return cost;
+    }
 
 
     /**
@@ -43,7 +134,7 @@ require(['underscore'], function (_) {
         this.seats = this.wantsToDrive < 0 ? 0 : _.random(2, 8);
         this.sex = _.random(0, 1);                   // Single-Sex cars are preferable; there tends to be less drama that way (at least from my perspective as a male)
         this.origin = _.random(1, locations.length - 1); // initial location, references "locations" var
-        this.inCar = -1; // default of 0.
+        this.inCar = -1; // default of -1, meaning unassigned.
     }
 
 
@@ -65,10 +156,19 @@ require(['underscore'], function (_) {
         this.addPassenger(driver);
     }
 
+
     Car.prototype.getDriver = function () {
         return this.passengers[0];
     };
     Car.prototype.__defineGetter__('driver', Car.prototype.getDriver);
+
+
+    Car.prototype.getCost = function () {
+        return evalCost(this, this.passengers);
+    };
+    Car.prototype.__defineGetter__('cost', Car.prototype.getCost);
+
+
     /**
      * The addPassenger method adds a passenger to a given car.
      *
@@ -94,9 +194,18 @@ require(['underscore'], function (_) {
      * @param {Person} addition The potential additional people.
      * @return {float} the cost difference
      */
-    Car.prototype.costDifferenceIfAdd = function (addition) {
+    Car.prototype.costDifferenceIfAdd = function (newPassenger) {
 
     };
+
+
+
+
+
+
+
+
+    /* THE ACTUAL PROCEDURE: */
 
 
     /* Initializes the test people */
@@ -106,7 +215,7 @@ require(['underscore'], function (_) {
     }
 
 
-    /* put all potential drivers in their cars.  */
+    /* Put all potential drivers in their cars.  */
     var canDrive = "has car";
     var cantDrive = "no car";
     people = _.groupBy(people, function (person) {
@@ -120,46 +229,57 @@ require(['underscore'], function (_) {
 
     /* For each non-driver, assign to best car */
 
+    people[cantDrive].forEach(function (passenger) {
+
+        cars = _.sortBy(cars, function(car) {
+            var passengers = [passenger].concat(car.passengers);
+            return evalCost(car, passengers)
+        });
+        cars[0].addPassenger(passenger);
+    });
+
 
 
     window.console.log("people", people);
 
     window.console.log("cars", cars);
 
+    window.console.info("Total Cost: ", evalTotalCost().toString());
+
 
 /* Old.  The following stuff should largely be ignored. */
 
 
-    // assign passengers to drivers (cars)
-    loop:
-    while (true) {
-        for (passengersInCar=0; ++passengersInCar < people[driver].seats;) {
-            if (driver >= passenger) {
-                break loop;
-            }
-            people[driver].inCarOf = people[driver].id;
-            people[passenger].inCarOf = people[driver].id;
-            passenger--;
-        }
-        driver++;
-    }
-
-
-    // split up the cars
-    people = _.groupBy(people, function(person) { return person.inCarOf; });
-
-
-    // add "empty seats" to cars
-    _.forEach(cars, function(car) {
-        while(car.length < _.first(car).seats) {
-            car.push(null);
-        }
-        document.write("did something!");
-        return this;
-    });
-
-
-    console.info(cars);
+    //// assign passengers to drivers (cars)
+    //loop:
+    //while (true) {
+    //    for (passengersInCar=0; ++passengersInCar < people[driver].seats;) {
+    //        if (driver >= passenger) {
+    //            break loop;
+    //        }
+    //        people[driver].inCarOf = people[driver].id;
+    //        people[passenger].inCarOf = people[driver].id;
+    //        passenger--;
+    //    }
+    //    driver++;
+    //}
+    //
+    //
+    //// split up the cars
+    //people = _.groupBy(people, function(person) { return person.inCarOf; });
+    //
+    //
+    //// add "empty seats" to cars
+    //_.forEach(cars, function(car) {
+    //    while(car.length < _.first(car).seats) {
+    //        car.push(null);
+    //    }
+    //    document.write("did something!");
+    //    return this;
+    //});
+    //
+    //
+    //console.info(cars);
 });
 
 
